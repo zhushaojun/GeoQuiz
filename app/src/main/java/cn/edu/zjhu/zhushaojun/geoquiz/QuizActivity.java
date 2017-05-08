12d7c5f1dc8ffe54a6f882b95acacd37c3fd781a
Shaojun Zhu <shj.zhu@gmail.com>
2017-05-08 08:22:09 +0800

    1.6.1

--- app/src/main/java/cn/edu/zjhu/zhushaojun/geoquiz/QuizActivity.java
+++ app/src/main/java/cn/edu/zjhu/zhushaojun/geoquiz/QuizActivity.java
@@ -12,5 +12,8 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
     }
 }
