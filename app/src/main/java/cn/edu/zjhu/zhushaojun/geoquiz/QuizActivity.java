70e4859369b2a4a9267d8215fffda10c87f122c2
Shaojun Zhu <shj.zhu@gmail.com>
2017-05-08 08:21:59 +0800

    1.6

--- app/src/main/java/cn/edu/zjhu/zhushaojun/geoquiz/QuizActivity.java
+++ app/src/main/java/cn/edu/zjhu/zhushaojun/geoquiz/QuizActivity.java
@@ -2,8 +2,11 @@ package cn.edu.zjhu.zhushaojun.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
