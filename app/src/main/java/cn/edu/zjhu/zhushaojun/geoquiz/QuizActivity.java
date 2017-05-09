f8fc55e473e3f789918f8b3d39dc5e0381b707a7
Shaojun Zhu <shj.zhu@gmail.com>
2017-05-09 08:22:07 +0800

    3.3

--- app/src/main/java/cn/edu/zjhu/zhushaojun/geoquiz/QuizActivity.java
+++ app/src/main/java/cn/edu/zjhu/zhushaojun/geoquiz/QuizActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -76,8 +77,18 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
